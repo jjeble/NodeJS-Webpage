651b3fac9eeec2a218e859b86e7cd8d2c6f486b8
jjeble <jayj2019@gmail.com>
2016-12-21 01:26:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe1.txt','utf-8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeMe1.txt','utf-8');
 
 myReadStream.on('data', function(chunk){
   console.log('new chunk recieved');
-  console.log(chunk);
+  myWriteStream.write(chunk);
 });
 
 
