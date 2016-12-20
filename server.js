ce91026e2814b771f5b810158597dc20992ee8ca
jjeble <jayj2019@gmail.com>
2016-12-21 01:16:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
+var myReadStream = fs.createReadStream(__dirname + '/readMe1.txt','utf-8');
+
+myReadStream.on('data', function(chunk){
+  console.log('new chunk recieved');
+  console.log(chunk);
+});
+
+
+
+
+
+
+/*
 var server = http.createServer(function(req,res){
   console.log('request was made: '+req.url);
   res.writeHead(200,{'Content-Type': 'text/plain'});
@@ -8,3 +22,4 @@ var server = http.createServer(function(req,res){
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to pport 3000');
+*/
