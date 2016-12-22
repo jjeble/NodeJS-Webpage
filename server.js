abafc64cac0ed72a5c3df98fa557e706040cf909
jjeble <jayj2019@gmail.com>
2016-12-23 01:42:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.get('/',function(req,res){
 app.get('/contact',function(req,res){
   res.send('this is the contact page');
 });
+app.get('/profile/:id',function(req,res){
+  res.send('this is the profile of id: '+req.params.id);
+});
 app.listen(3000);
