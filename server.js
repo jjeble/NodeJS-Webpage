fcc13005ffb85c15bd151ca4fcd586b010f07ebf
jjeble <jayj2019@gmail.com>
2016-12-22 22:45:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,11 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
-//var myReadStream = fs.createReadStream(__dirname + '/readMe1.txt','utf-8');
-//var myWriteStream = fs.createWriteStream(__dirname + '/writeMe1.txt','utf-8');
+var app = express();
 
-
-//myReadStream.pipe(myWriteStream);
-var server = http.createServer(function(req,res){
-  console.log('request was made: '+req.url);
-  if(req.url === '/home'||req.url === '/'){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    fs.createReadStream(__dirname+'/index.html').pipe(res);
-
-  }
-  else if(req.url === '/contact-us') {
-
-        res.writeHead(200,{'Content-Type': 'html'});
-        fs.createReadStream(__dirname+'/contact.html').pipe(res);
-
-
-  }
-  else if(req.url==='/api/ninjas'){
-    var ninjas  = [{name:'ryu', age:29},{name:'yoshi',age:32}];
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.end(JSON.stringify(ninjas));
-  }
-  else {
-
-      res.writeHead(200,{'Content-Type': 'html'});
-      fs.createReadStream(__dirname+'/404.html').pipe(res);
-
-  }
-  //res.writeHead(200,{'Content-Type': 'application/json'});
-  //var myObj = {name: 'Ryu',
-              //job: 'Ninja',
-            //age: 29};
-
-
-
-  //var myReadStream = fs.createReadStream(__dirname + '/index.html','utf-8');
-  //myReadStream.pipe(res);
-  //res.end('Hey guys');
+app.get('/',function(req,res){
+  res.send('this is th homepage');
 });
-
-server.listen(3000, '127.0.0.1');
-console.log('listening to pport 3000');
+app.get('/contact',function(req,res){
+  res.send('this is the contact page');
+});
+app.listen(3000);
