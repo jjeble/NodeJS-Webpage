1a59ba323a622f2b2ac8286093b0ce138ec592e4
jjeble <jayj2019@gmail.com>
2016-12-31 12:18:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 app.set('view engine', 'ejs');
 app.use('/assets',express.static('assets'));
 app.get('/',function(req,res){
@@ -10,6 +12,11 @@ app.get('/',function(req,res){
 app.get('/contact',function(req,res){
 
   res.render('contact',{qs: req.query});
+});
+
+app.post('/contact',urlencodedParser,function(req,res){
+
+  res.render('contact-success',{data:req.body});
 });
 app.get('/profile/:name',function(req,res){
   var data = {age:29, job: 'ninja', hobbies:['eating','shopping','fishing']};
